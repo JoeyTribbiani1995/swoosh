4dce9efd1349a8dc3ea84fabc0221db2d9e7c06b
JoeyTribbiani <vophanhongdung@gmail.com>
2018-03-12 16:28:06 +0700

    commit practice auto layout

--- swoosh/swoosh/ViewController.swift
+++ swoosh/swoosh/ViewController.swift
@@ -18,9 +18,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
-        swooshLogo.frame = CGRect(x: view.frame.size.width/2 - swooshLogo.frame.size.width/2, y: 50, width: swooshLogo.frame.size.width, height: swooshLogo.frame.size.height)
-        
-        beginScreenBG.frame = view.frame
+//        swooshLogo.frame = CGRect(x: view.frame.size.width/2 - swooshLogo.frame.size.width/2, y: 50, width: swooshLogo.frame.size.width, height: swooshLogo.frame.size.height)
+//        
+//        beginScreenBG.frame = view.frame
     }
 
     override func didReceiveMemoryWarning() {
