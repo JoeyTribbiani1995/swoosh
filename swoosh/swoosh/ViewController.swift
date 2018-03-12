d4329ff4a9b17fbd3b2db4d316c0c37138b6be43
JoeyTribbiani <vophanhongdung@gmail.com>
2018-03-12 15:16:35 +0700

    working with frames

--- swoosh/swoosh/ViewController.swift
+++ swoosh/swoosh/ViewController.swift
@@ -10,14 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var swooshLogo: UIImageView!
+    @IBOutlet weak var beginScreenBG: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
+        swooshLogo.frame = CGRect(x: view.frame.size.width/2 - swooshLogo.frame.size.width/2, y: 50, width: swooshLogo.frame.size.width, height: swooshLogo.frame.size.height)
+        
+        beginScreenBG.frame = view.frame
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
 
